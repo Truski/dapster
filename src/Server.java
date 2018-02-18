0d6da7e0efc32327d45df37c5f502efd12bf4b73
Daniel Wojtowicz <dwojtowi@hawk.iit.edu>
2018-02-18 16:18:51 -0600

    Replace peer with peerid in register signature

--- src/Server.java
+++ src/Server.java
@@ -21,18 +21,18 @@ public class Server {
   /**
    * Adds the Peer-Filename combination to the Indexing registry.
    *
-   * @param peer PeerID (Address, Port) of the file to add
+   * @param peerid PeerID (Address, Port) of the file to add
    * @param filename Name of the file that that Peer has
    */
-  public synchronized void register(Peer peer, String filename){
+  public synchronized void register(Peer peerid, String filename){
     // Check if the filename is in the HashMap
     if(registry.containsKey(filename)){
       // If the filename is in the index, just add the new Peer to the list
-      registry.get(filename).add(peer);
+      registry.get(filename).add(peerid);
     } else {
       // If the filename is not in the index, make a new list and add the new Peer to it
       ArrayList<Peer> peers = new ArrayList<Peer>();
-      peers.add(peer);
+      peers.add(peerid);
 
       // Add the new list to the index
       registry.put(filename, peers);
