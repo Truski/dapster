0def19daf83d9173017fa7a40a9e316595cf92d8
Daniel Wojtowicz <dwojtowi@hawk.iit.edu>
2018-02-18 16:14:04 -0600

    Add comments to Server

--- src/Server.java
+++ src/Server.java
@@ -1,26 +1,50 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * This class contains the implementation for a Server. It maintains a HashMap that maps file names to a list of peers
+ * that have that file.
+ */
+
 public class Server {
 
   public static final int PORT = 1888;
   public HashMap<String, ArrayList<Peer>> registry;
 
-  public Server(){
-    registry = new HashMap<String, ArrayList<Peer>>();
+  /**
+   * Creates a Server object with an empty registry.
+   */
+  public Server() {
+    this.registry = new HashMap<String, ArrayList<Peer>>();
   }
 
+  /**
+   * Adds the Peer-Filename combination to the Indexing registry.
+   *
+   * @param peer PeerID (Address, Port) of the file to add
+   * @param filename Name of the file that that Peer has
+   */
   public synchronized void register(Peer peer, String filename){
+    // Check if the filename is in the HashMap
     if(registry.containsKey(filename)){
+      // If the filename is in the index, just add the new Peer to the list
       registry.get(filename).add(peer);
     } else {
+      // If the filename is not in the index, make a new list and add the new Peer to it
       ArrayList<Peer> peers = new ArrayList<Peer>();
       peers.add(peer);
+
+      // Add the new list to the index
       registry.put(filename, peers);
     }
   }
 
-  public ArrayList<Peer> search(String filename) {
+  /**
+   * Returns a list of Peers that have the the file with the given name.
+   * @param filename Name of the file to search for
+   * @return Returns a list of peers that have that file. Returns null if no peer has the file.
+   */
+  public synchronized ArrayList<Peer> search(String filename) {
     return registry.get(filename);
   }
 }
